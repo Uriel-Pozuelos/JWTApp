c2a12d3e1d36aec32da60f626476c73cae9fe651
UrielP <21000013@alumnos.utleon.edu.mx>
2024-08-05 23:45:19 -0600

    jwt app

---
+++ client/src/app/services/role.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Role } from '../interfaces/role';
+import { RoleCreateRequest } from '../interfaces/role-create-request';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoleService {
+  apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getRoles = (): Observable<Role[]> =>
+    this.http.get<Role[]>(`${this.apiUrl}Roles`);
+
+  createRole = (role: RoleCreateRequest): Observable<{ message: string }> =>
+    this.http.post<{ message: string }>(`${this.apiUrl}Roles`, role);
+
+  delete = (id: string): Observable<{ message: string }> =>
+    this.http.delete<{ message: string }>(`${this.apiUrl}Roles/${id}`);
+
+  assignRole = (
+    userId: string,
+    roleId: string
+  ): Observable<{ message: string }> =>
+    this.http.post<{ message: string }>(`${this.apiUrl}Roles/assign`, {
+      userId,
+      roleId,
+    });
+}
